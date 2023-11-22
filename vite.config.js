f5833bfc8d9fc197e71705a4c4d4f53f0c9ac64d
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-11-22 16:20:24 -0300

    added router base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/vue-composable-store",
+  router: {
+    base: "/vue-composable-store/"
+  },
   plugins: [
     vue({
       template: { transformAssetUrls },
